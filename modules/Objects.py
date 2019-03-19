a5ae3d5ce7a62d03b8364b4ff9e0ddd93b07678e
Daniel Mishler <dasmish@iu.edu>
2019-03-19 13:23:17 -0400

    basic scaffold and environment

--- modules/Objects.py
+++ modules/Objects.py
@@ -5,3 +5,20 @@ Created on Tue Mar 19 13:12:28 2019
 @author: dsmis
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+lattice_len = 10
+lattice_height = 10
+class Environment:
+    def __init__(self):
+        self.hidden_variable = False # cause Python
+        self.agent_ID = np.zeros((lattice_len,lattice_height)) # represent where agents are located
+        for i in range(0, self.agent_ID.shape[0]):
+            for j in range(0, self.agent_ID.shape[1]):
+                self.agent_ID[i][j] = 0
+        return
+    def plot_me(self):
+        plt.figure()
+        plt.contourf(np.transpose(self.agent_ID))
+        return
--- modules/Scaffold.py
+++ modules/Scaffold.py
@@ -4,4 +4,10 @@ Created on Tue Mar 19 13:07:35 2019
 
 @author: dsmis
 """
+import Objects
 
+def startup():
+    grid = Objects.Environment()
+    grid.plot_me()
+
+startup()
