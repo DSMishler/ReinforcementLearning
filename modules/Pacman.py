5962bdb9695a17383f39ed984801f0f1b9e3a84b
Eric Bower <ebower42@gmail.com>
2019-03-26 13:24:15 -0400

    Pacman.py

---
+++ modules/Pacman.py
@@ -0,0 +1,13 @@
+class Pacman:
+    def __init__(self, grid):
+        self.pos_x = 0
+        self.pos_y = 0
+        self.neighbours = [[1,0],[0,1], [-1,0], [0,-1]]
+        return
+
+    def move_me(self, dest_x, dest_y):
+        self.pos_x = dest_x
+        self.pos_y = dest_y
+        return
+
+    def move(self, grid):
