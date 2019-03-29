69e79b7760418603d6ed639d5afc46153d113864
Daniel Mishler <dasmish@iu.edu>
2019-03-29 17:20:11 -0400

    also now we plot said dots. More quality of life changes

--- modules/PacPlots.py
+++ modules/PacPlots.py
@@ -6,12 +6,41 @@ Created on Tue Mar 26 13:19:13 2019
 """
 
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import (AnnotationBbox, OffsetImage)
+from matplotlib.cbook import get_sample_data
 import numpy as np
 
 def plot_all(grid, ghost, pacman, filename):
+    
+    '''
+    An attempt to make it work :/
+    # get pacman!
+    fig, ax = plt.subplots()
+    
+    pacIm = get_sample_data('PacmanImg.png',asfileobj=False)
+    arr_image = plt.imread(pacIm, format='png')
+    
+    imagebox = OffsetImage(arr_image, zoom=0.3)
+    imagebox.image.axes = ax
+    
+    pos = [pacman.pos_x, pacman.pos_y]
+    box = AnnotationBbox(imagebox, pos, xybox=(120., -80.),
+                         xcoords = 'data',
+                         boxcoords = "offset points",
+                         pad = .5,
+                         arrowprops = dict(
+                                 arrowstyle="->",
+                                 connectionstyle="angle,angleA=0,angleB=90,rad=3")
+                         )
+    ax.add_artist(box)
+    '''
     fig = plt.figure( figsize = (10,10))
     cs = plt.contourf(np.transpose(grid.agent_ID))
     plt.axis('image')
+    for i in range(grid.pellet_ID.shape[0]):
+        for j in range(grid.pellet_ID.shape[1]):
+            if grid.pellet_ID[i,j] == 1:
+                plt.plot(i,j,'wo')
     plt.plot(ghost.pos_x, ghost.pos_y, 'ro')
     plt.plot(pacman.pos_x, pacman.pos_y, 'yo')
     plt.savefig(filename)
